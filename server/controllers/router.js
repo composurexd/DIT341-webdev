2719dc41277726b2608031e047766be2d9188523
maxfri <maxfri@student.chalmers.se>
2020-09-16 18:11:56 +0200

    added model skeliton

--- server/controllers/router.js
+++
@@ -1,2 +0,0 @@
-const companyHandler = require("./companyHandler");
-const 
---
+++ server/controllers/routerAPI.js
@@ -0,0 +1,21 @@
+const companyHandler = require("./companyHandler");
+const employeeHandler = require("./employeeHandler");
+const expencesHandler = require("./expencesHandler");
+const tripHandler = require("./tripsHandler");
+const router = express.router();
+
+router.use("/")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
