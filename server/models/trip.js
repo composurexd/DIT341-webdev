7592ef18ad8f54f75c56e170a4cf6d0702331796
maxfri <maxfri@student.chalmers.se>
2020-09-17 21:00:22 +0200

    fixed git branches?

--- server/models/trip.js
+++ server/models/trip.js
@@ -8,10 +8,6 @@ var tripSchema = new Schema({
     budget: {type: Number, required: true, default: 0}
 })
 
-<<<<<<< HEAD
 var Trip = mongoose.model("trips", tripSchema);
-=======
-var Trip = mongoose.model("trip", tripSchema);
->>>>>>> fixed some issues with commiting nonsence lines
 
 module.exports = Trip;
