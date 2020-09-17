c5f8815f588a640b1cb62e35bded3aa8b8c50a1c
berjo <gusberjofx@student.gu.se>
2020-09-17 20:38:42 +0200

    fix conflicts

--- server/models/trip.js
+++ server/models/trip.js
@@ -8,10 +8,6 @@ var tripSchema = new Schema({
     budget: {type: Number, required: true, default: 0}
 })
 
-<<<<<<< HEAD
 var Trip = mongoose.model("trips", tripSchema);
-=======
-var Trip = mongoose.model("trip", tripSchema);
->>>>>>> 0adba28c1a815ccecdda5747727b16fa4d23f09f
 
 module.exports = Trip;
