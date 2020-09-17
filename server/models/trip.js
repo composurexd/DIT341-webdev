a5a59c4c47a54c19ffa9b7fd23711a49f2ef3ecd
berjo <gusberjofx@student.gu.se>
2020-09-18 01:02:54 +0200

    update

--- server/models/trip.js
+++ server/models/trip.js
@@ -6,11 +6,8 @@ var tripSchema = new Schema({
     description: {type: String, require:true, default: "NULL Discription"},
     date: {type: Date, default: Date.now, require:true},
     budget: {type: Number, required: true, default: 0},
-    expense: [{type: Schema.Types.ObjectId, ref:"expense"}]
-<<<<<<< HEAD
+    expense: [{type: Schema.Types.ObjectId, ref:"Expense"}]
 
-=======
->>>>>>> cc223badfb0a815224fd1aaace9ea7562dc7aa5f
 })
 
 var Trip = mongoose.model("trips", tripSchema);
