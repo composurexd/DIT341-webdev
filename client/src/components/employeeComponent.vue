4ee59a490411712534b0e8fb3b0f685fc429aa22
maxfri <maxfri@student.chalmers.se>
2020-09-30 23:26:06 +0200

    employees can be displayed now (rudimentally)

---
+++ client/src/components/employeeComponent.vue
@@ -0,0 +1,45 @@
+<template>
+    <b-list-group>
+        <b-list-group-item>
+            <b-container>
+                <b-row>
+                    <b-col>
+                        {{employee.fname}}
+                    </b-col>
+                    <b-col>
+                        {{employee.lname}}
+                    </b-col>
+                    <b-col>
+                        {{employee.birthday}}
+                    </b-col>
+                    <b-col>
+                        {{employee.userName}}
+                    </b-col>
+                    <b-col>
+                        {{employee.userPass}}
+                    </b-col>
+                    <b-col>
+                        {{employee.trips}}
+                    </b-col>
+                </b-row>
+            </b-container>
+        </b-list-group-item>
+    </b-list-group>
+</template>
+
+<script>
+export default {
+  name: 'aEmployee',
+  props: ['employee'],
+
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+    color: red;
+}
+</style>
--- client/src/views/Potato.vue
+++ client/src/views/Potato.vue
@@ -1,46 +1,38 @@
 <template>
   <div>
     <h1> THIS IS THE TABLE OF EMPLOYEES</h1>
-    <b-table striped hover :items="items" :fields="fields" @click= "clickEmployee()"></b-table>
+    <li v-for='employee in employees' :key='employee._id'>
+        <aEmployee :employee='employee' />
+    </li>
   </div>
 </template>
 
 <script>
+import aEmployee from '@/components/employeeComponent.vue'
+import { Api } from '@/Api.js'
+
 export default {
+  name: 'EmployeeView',
+  components: {
+    aEmployee
+  },
+
   data() {
     return {
-      // Note 'isActive' is left out and will not appear in the rendered table
-      fields: [
-        {
-          key: 'ID',
-          label: 'Employee ID',
-          sortable: true
-        },
-        {
-          key: 'first_name',
-          sortable: true
-        },
-        {
-          key: 'last_name',
-          sortable: true
-        },
-        {
-          key: 'birthDate',
-          sortable: true
-        }
-      ],
-      items: [
-        { ID: 'DSADSA54896DS4A89A', first_name: 'Dickerson', last_name: 'Macdonald', birthDate: new Date() },
-        { ID: '478DSA498A48DSA78D', first_name: 'Larsen', last_name: 'Shaw', birthDate: new Date() },
-        { ID: '48D7SA489DS484A948', first_name: 'Geneva', last_name: 'Wilson', birthDate: new Date() },
-        { ID: '9489SD7A9D7ADA79DS', first_name: 'Jami', last_name: 'Carney', birthDate: new Date() }
-      ],
-      hover: true
-      // stacked <- to allow for small screens
+      employees: []
     }
   },
+  created() {
+    this.getEmployees()
+  },
   methods: {
-    clickEmployee() {
+    getEmployees() {
+      this.employees = []
+      Api.get('/employee').then(response => {
+        this.employees = []
+        this.employees = response.data.employees
+        console.log(this.employees)
+      })
     }
   }
 }
